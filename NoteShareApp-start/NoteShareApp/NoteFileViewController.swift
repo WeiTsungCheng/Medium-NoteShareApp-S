fbdc6487fad587853b9299a4d88d3fac5ad40603
weitsungcheng <sanctuary85c@gmail.com>
2017-12-10 20:01:56 +0800

    add NoteShareApp-start

---
+++ NoteShareApp-start/NoteShareApp/NoteFileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  NoteFileViewController.swift
+//  NoteShareApp
+//
+//  Created by 鄭煒宗 on 2017/11/18.
+//  Copyright © 2017年 鄭煒宗. All rights reserved.
+//
+
+import UIKit
+
+class NoteFileViewController: UIViewController {
+    
+    @IBOutlet weak var notetitle: UITextField!
+    @IBOutlet weak var noteContain: UITextView!
+    
+    @IBAction func shareNote(_ sender: Any) {
+        
+        }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
